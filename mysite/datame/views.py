3fc1bc9401fb0c98e7ca9b0d90c154271d0e3197
Iván De los Santos García <ivandega22@gmail.com>
2019-03-20 21:14:28 +0100

    GET example

--- mysite/datame/views.py
+++ mysite/datame/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def hi(request):
+    if request.method == 'GET':
+        data = {"response": "hi"}
+        return JsonResponse(data)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from datame.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("hi/", hi, name="hi"),
 ]
