985a0becb44449463da2c2ef06e1b06cb7dc6c02
Iván De los Santos García <ivandega22@gmail.com>
2019-03-21 12:27:54 +0100

    Create offer

--- mysite/datame/models.py
+++ mysite/datame/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Offer_model(models.Model):
+    CURRENCY_CHOICES = (
+        ('0', '€'),
+        ('1', '$'),
+        ('2', '£')
+    )
+
+    title = models.CharField('Offer title', max_length = 80)
+    description = models.TextField('Offer description')
+    price_offered = models.FloatField('Price offered')
+    currency = models.CharField('Currency type',max_length = 1, choices = CURRENCY_CHOICES)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    limit_time = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title
--- mysite/datame/views.py
+++ mysite/datame/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import Offer_model
+import pytz, datetime
 
 # Create your views here.
+@csrf_exempt
+def Offer(request):
+    if request.method == "POST":
+        data = request.POST
+        title = data['title']
+        description = data['description']
+        price_offered = data['price_offered']
+        currency = data['currency']
+        limit_time = data['limit_time']
+
+        # Time management
+        split_time = limit_time.split(',') # Split by comma what was sent from client 
+        split_time = list(map(int, split_time)) # Convert from list of string to list of integers
+
+        if len(split_time) == 7:
+            date = datetime.datetime(split_time[0], split_time[1], 
+                split_time[2], split_time[3], split_time[4], split_time[5], split_time[6], pytz.UTC)
+        
+        # Creation of new offer
+        new_offer = Offer_model.objects.create(title=title, description=description, 
+            price_offered=float(price_offered), currency=currency, limit_time=date)
+
+        print('Sucessfully created new offer')
+        return JsonResponse({"message":"Successfully created new offer"})
